d924fea21a2de84f1298233808576b704b6c938f
Josh <ejosh.manuel@gmail.com>
2019-03-30 13:22:47 -0700

    schema and seeds added

--- Unsolved/models/schema.sql
+++ Unsolved/models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS hikersdb;
+CREATE DATABASE hikersdb;
+USE hikersdb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- Created the table "users" --
+CREATE TABLE users
+(
+  id INT AUTO_INCREMENT,
+  email VARCHAR(30) NOT NULL,
+  username VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  location VARCHAR(30) NOT NULL,
+  passport VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
