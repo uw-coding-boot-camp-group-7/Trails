6a58ecb2ae5d572f299dfdb96f4d1c7b8c31fa26
NAYWOOD <naywood@D-10-19-139-109.dhcp4.washington.edu>
2019-04-03 19:06:55 -0700

    made env

--- models/index.js
+++ models/index.js
@@ -7,6 +7,9 @@ var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
+config.password =  process.env.DB_PASS;
+
+
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
