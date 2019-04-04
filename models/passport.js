d36e8745f2ac893621b6146b0a69bc22f99a6fbf
Josh <ejosh.manuel@gmail.com>
2019-04-03 18:36:25 -0700

    fixed associations; updated passport model

--- models/passport.js
+++ models/passport.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var Passport = sequelize.define("Passport", {
     photo: {
-      type: DataTypes.LONGBLOB
+      type: DataTypes.BLOB
     },
     location: {
       type: DataTypes.STRING,
