b45bd49f0023ebb3d228d13a67e3320dd13d2140
Joonwoo Choi <joon3703@gmail.com>
2019-04-06 14:31:28 -0700

    user input works

--- public/js/passportCreate.js
+++ public/js/passportCreate.js
@@ -3,9 +3,23 @@ $(document).ready(function() {
     // Getting a reference to the passport 
     var $getNewPassport = $("get.new-passport");
     // Getting a wish trails and will go to wish trails container
-    var $wishTrails = $("wish-trails");
+    var $getWishTrails = $("get.wish-trails");
     // Getting a hiked trails and will go to hiked trails container
-    var $hikedTrails = $("hiked-trails");
+    var $getHikedTrails = $("get.hiked-trails");
+
+    // Adding event listeners for completing, and deleting trails
+    $(document).on("click", "button.complete", toggleComplete);
+    $(document).on("click", "button.delete", deleteTrails);
+
+    // Our initial wish and hiked trails
+    var wishTrails = [];
+    var hikedTrails = [];
+
+    // Initializing rows
+    function initializeRows() {
+        $getWishTrails.empty();
+        var rowsToAdd = [];
+    }
 
 
 
